267c930a8def56f6409e65417c01c162997182c9
RohitSarode1 <128139546+RohitSarode1@users.noreply.github.com>
2023-03-23 09:03:33 +0530

    Add files via upload

---
+++ pattern/Pattern9.java
@@ -0,0 +1,18 @@
+class Pattern9
+{
+   public static void main(String args[])
+   {
+       for(int i=1;i<=6;i++)
+	   {
+		   for(int k=1;k<=i;k++)
+		   {
+			   System.out.print(" ");
+		   }
+	      for (int j=1;j<=5;j++)
+		  {
+		     System.out.print("* ");
+		  }
+		  System.out.println();
+	   }
+   }
+}
